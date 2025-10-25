d338fed3be3d5b664814bdf171e8ede8963c9e66
User <User@MacBook-Pro.local>
2025-10-25 16:17:53 +0300

    fix warning eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
   pluginJs.configs.recommended,
+  stylistic.configs.recommended,
   {
     files: ['**/*.js'],
     languageOptions: {
       globals: globals.node,
     },
     rules: {
-      'arrow-parens': ['error', 'as-needed'],
+      'arrow-parens': 'off',
+      '@stylistic/arrow-parens': ['error', 'as-needed'],
       'quotes': ['error', 'single'],
       'semi': ['error', 'never'],
     },
